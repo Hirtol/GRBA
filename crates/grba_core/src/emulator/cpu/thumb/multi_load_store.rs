eb37b393826bd121a2ae1374c4acca6b2483c99b
Valentijn Hol <valentijnhol@hotmail.nl>
2022-04-05 22:22:04 +0200

    Split multiple load and multiple store

--- crates/grba_core/src/emulator/cpu/thumb/multi_load_store.rs
+++ crates/grba_core/src/emulator/cpu/thumb/multi_load_store.rs
@@ -50,33 +50,17 @@ impl ThumbV4 {
     }
 
     pub fn multiple_store(cpu: &mut CPU, instruction: ThumbInstruction, bus: &mut Bus) {
-        // For the duration of this instruction PC will be 6 ahead instead of just 4.
-        cpu.registers.general_purpose[PC_REG] += 2;
-        Self::multiple_load_store(cpu, instruction, bus, false);
-        cpu.registers.general_purpose[PC_REG] -= 2;
-    }
-
-    pub fn multiple_load(cpu: &mut CPU, instruction: ThumbInstruction, bus: &mut Bus) {
-        Self::multiple_load_store(cpu, instruction, bus, true);
-    }
-
-    #[inline(always)]
-    fn multiple_load_store(cpu: &mut CPU, instruction: ThumbInstruction, bus: &mut Bus, is_load: bool) {
         let r_base = instruction.get_bits(8, 10) as usize;
-
         let register_list = instruction.get_bits(0, 7) as u8;
+
         let mut base_address = cpu.read_reg(r_base);
-        let mut has_writeback = true;
 
         // Handle edge case of empty register list.
         if register_list == 0 {
+            // PC is +2 further ahead than usual during this edge case
             // Empty register list is interpreted as PC_REG being transferred.
-            if is_load {
-                cpu.write_reg(PC_REG, bus.read_32(base_address, cpu), bus);
-            } else {
-                let reg_value = cpu.read_reg(PC_REG);
-                bus.write_32(base_address, reg_value);
-            }
+            let reg_value = cpu.read_reg(PC_REG) + 2;
+            bus.write_32(base_address, reg_value);
 
             // When the register list is empty we add 0x40 to the base address.
             cpu.write_reg(r_base, base_address + 0x40, bus);
@@ -86,43 +70,57 @@ impl ThumbV4 {
 
         // Edge cases for where the base register is included in the store/load register list
         if register_list.check_bit(r_base as u8) {
-            if is_load {
-                // In ARMv4/v5 writeback gets disabled
-                has_writeback = false
-            } else {
-                // In ARMv4 we store the old base if the register is *first* in the list, otherwise store new base
-                // In ARMv5 we always store the new base
-                let registers = register_list << (7 - r_base);
+            // In ARMv4 we store the old base if the register is *first* in the list, otherwise store new base
+            // In ARMv5 we always store the new base
+            let registers = register_list << (7 - r_base);
 
-                // Is not first in the list, then we store the new base
-                if registers.count_ones() != 1 {
-                    // Store the NEW base ahead of time
-                    let final_address = base_address.wrapping_add(4 * register_list.count_ones());
-                    cpu.write_reg(r_base, final_address, bus);
-                }
+            // Is not first in the list, then we store the new base
+            if registers.count_ones() != 1 {
+                // Store the NEW base ahead of time
+                let final_address = base_address.wrapping_add(4 * register_list.count_ones());
+                cpu.write_reg(r_base, final_address, bus);
             }
         }
 
-        if is_load {
-            for i in 0..8 {
-                if register_list.check_bit(i) {
-                    cpu.write_reg(i as usize, bus.read_32(base_address, cpu), bus);
+        for i in 0..8 {
+            if register_list.check_bit(i) {
+                let reg_value = cpu.read_reg(i as usize);
+                bus.write_32(base_address, reg_value);
 
-                    base_address = base_address.wrapping_add(4);
-                }
-            }
-        } else {
-            for i in 0..8 {
-                if register_list.check_bit(i) {
-                    let reg_value = cpu.read_reg(i as usize);
-                    bus.write_32(base_address, reg_value);
-
-                    base_address = base_address.wrapping_add(4);
-                }
+                base_address = base_address.wrapping_add(4);
             }
         }
 
-        if has_writeback {
+        cpu.write_reg(r_base, base_address, bus);
+    }
+
+    pub fn multiple_load(cpu: &mut CPU, instruction: ThumbInstruction, bus: &mut Bus) {
+        let r_base = instruction.get_bits(8, 10) as usize;
+        let register_list = instruction.get_bits(0, 7) as u8;
+
+        let mut base_address = cpu.read_reg(r_base);
+
+        // Handle edge case of empty register list.
+        if register_list == 0 {
+            // Empty register list is interpreted as PC_REG being transferred.
+            cpu.write_reg(PC_REG, bus.read_32(base_address, cpu), bus);
+            // When the register list is empty we add 0x40 to the base address.
+            cpu.write_reg(r_base, base_address + 0x40, bus);
+
+            return;
+        }
+
+        for i in 0..8 {
+            if register_list.check_bit(i) {
+                cpu.write_reg(i as usize, bus.read_32(base_address, cpu), bus);
+
+                base_address = base_address.wrapping_add(4);
+            }
+        }
+
+        // Edge cases for where the base register is included in the store/load register list
+        // In ArmV4/ArmV5 write-back gets disabled
+        if !register_list.check_bit(r_base as u8) {
             cpu.write_reg(r_base, base_address, bus);
         }
     }
